a11d8a281c6db52425417d7148ef4c94fee708b6
Javi <javierlopez1@iesebre.com>
2015-10-13 21:07:00 +0200

    MVC versió 2

--- main.php
+++ main.php
@@ -15,6 +15,7 @@ class Model
     }
 }
 class View
+class View
 {
     private $model;
     private $controller;
@@ -24,20 +25,29 @@ class View
         $this->model = $model;
     }
 
-    public function output(){
-        return "<p>" . $this->model->string . "</p>";
+    public function output() {
+        return '<p><a href="main.php?action=clicked"' . $this->model->string . "</a></p>";
     }
 }
 class Controller
 {
     private $model;
 
-    public function __construct($model) {
+    public function __construct($model){
         $this->model = $model;
+    }
+
+    public function clicked() {
+        $this->model->string = “Updated Data, thanks to MVC and PHP!”
     }
 }
 
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+}
+
 echo $view->output();
